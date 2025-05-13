d5d41cc5adc3cd4c5223705f31974955b870ea9b
e-sparrow <51369840+e-sparrow@users.noreply.github.com>
2025-05-13 22:09:12 +0300

    init

---
+++ Enums/ESynonymTemperature.cs
@@ -0,0 +1,12 @@
+﻿namespace Birdhouse.Extended.Synonyms.Enums
+{
+    public enum ESynonymTemperature
+    {
+        Formal,
+        Neutral,
+        Affectionate,
+        Intimate,
+        Playful,
+        Diminutive
+    }
+}
---
+++ Unity/SynonymHelper.cs
@@ -0,0 +1,22 @@
+using System;
+using Birdhouse.Extended.Synonyms.Unity;
+
+namespace Birdhouse.Extended.Synonyms
+{
+    public static class SynonymHelper
+    {
+        private static readonly Lazy<SynonymStorage> LazyStorage = new Lazy<SynonymStorage>(CreateStorage);
+
+        public static SynonymStorage Storage => LazyStorage.Value;
+
+        public static SynonymStorage CreateStorage()
+        {
+            var sheet = SynonymSettings.instance.SynonymsSheet;
+            
+            var storage = new SynonymStorage(sheet.text);
+            storage.Initialize();
+
+            return storage;
+        }
+    }
+}
